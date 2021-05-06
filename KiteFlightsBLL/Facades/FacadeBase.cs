6be4aa82548751f0792d5cd96b8fe01df7a21fc8
pepper <peerplamel@gmail.com>
2021-05-06 22:53:52 +0300

    Extended FacadeBase
    - Added a ctor that receives NpgsqlConnection as parameter, and initiates all the DAOs (and assigns the connection to a private field).
    - Added implementation of IDisposable to be able to dispose of the connection.

--- KiteFlightsBLL/Facades/FacadeBase.cs
+++ KiteFlightsBLL/Facades/FacadeBase.cs
@@ -1,10 +1,14 @@
 ﻿using KiteFlightsCommon.DaoInterfaces;
+using KiteFlightsDAL.DAOs.PocoDaos;
+using Npgsql;
 using System;
 
 namespace KiteFlightsBLL.Facades
 {
-	public abstract class FacadeBase
+	public abstract class FacadeBase : IDisposable
 	{
+		private readonly NpgsqlConnection _connection;
+
 		protected ICountryDao _countryDao;
 		protected IUserDao _userDao;
 		protected IAdminDao _adminDao;
@@ -12,5 +16,23 @@ namespace KiteFlightsBLL.Facades
 		protected IAirlineDao _airlineDao;
 		protected IFlightDao _flightDao;
 		protected ITicketDao _ticketDao;
+
+		public FacadeBase(NpgsqlConnection connection)
+		{
+			_connection = connection;
+
+			_countryDao = new CountryDaoPgsql(_connection);
+			_userDao = new UserDaoPgsql(_connection);
+			_adminDao = new AdminDaoPgsql(_connection);
+			_customerDao = new CustomerDaoPgsql(_connection);
+			_airlineDao = new AirlineDaoPgsql(_connection);
+			_flightDao = new FlightDaoPgsql(_connection);
+			_ticketDao = new TicketDaoPgsql(_connection);
+		}
+
+		public void Dispose()
+		{
+			_connection.Dispose();
+		}
 	}
 }
